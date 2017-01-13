31870c61f1c9083cd37912c9175de72c720fe154
offme20 <offamp@gmail.com>
2017-01-13 11:53:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -12,22 +12,26 @@ require_once('corebot.php');
 $channelAccessToken = 'e1MrLtknhZ66lKs5h1DRAphwwyg9ra+oOTA0wTMx4J1h19cD1yE/b9OKsHzK9qiRJQd8JT0/HBOJ+ZfR6yhTQalvcAr7jvbiMsfSI0CvFUytlY8GceZQtwkLsGiZ+OWy9omqCKYPKgq68vKEPB6axgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '01499f7264759f7ab07bb96a2eb63279';
 
-$client = new corebot($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+//$client = new corebot($channelAccessToken, $channelSecret);
+foreach ($bot->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
             switch ($message['type']) {
                 case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']
-                            )
-                        )
-                    ));
+                    //$client->replyMessage(array(
+                    //    'replyToken' => $event['replyToken'],
+                    //    'messages' => array(
+                    //        array(
+                    //            'type' => 'text',
+                    //            'text' => $message['text']
+                    //        )
+                    //    )
+                    //));
+                    $response = $bot->replyText($channelAccessToken, 'hello!');
                     break;
                 default:
                     error_log("Unsupporeted message type: " . $message['type']);
