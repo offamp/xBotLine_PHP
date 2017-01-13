78aacb0da957d98efcd85233eabee1ce1af4d666
offme20 <offamp@gmail.com>
2017-01-13 10:37:36 +0700

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LINE;
+
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\MessageType;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\Util\DummyHttpClient;
+
+require_once('corebot.php');
+
+$channelAccessToken = '<your channel access token>';
+$channelSecret = '<your channel secret>';
+
+$client = new corebot($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
